e7e499586e43055c0ae40237a90e1e2721d354af
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-07-30 12:31:15 +0200

    Esercizio bonus fase 1

---
+++ allSettled.js
@@ -0,0 +1,76 @@
+// getDashboardData(query)
+// http://localhost:3333/destinations?search=Kyoto RICERCA CITTA'
+
+console.log("AVVIO");
+const URLCOUNTRY = `http://localhost:3333/destinations`;
+const URLMETEO = `http://meteofittizio.it`;
+const URLAIRPORT = `http://localhost:3333/airports`;
+
+async function fetchJson(url) {
+  const response = await fetch(url);
+  const result = await response.json();
+  return result;
+}
+
+async function getDashboardData(query) {
+  // ---- Definiamo le Promise
+  const country = fetchJson(`${URLCOUNTRY}?search=${query}`);
+  const meteo = fetchJson(`${URLMETEO}?search=${query}`);
+  const airports = fetchJson(`${URLAIRPORT}`);
+
+  // ---- Iniziamo le Promises
+  const info = await Promise.allSettled([country, meteo, airports]);
+  const [countryResult, meteoResult, airportsResult] = info;
+
+  console.log(countryResult);
+  console.log(meteoResult);
+  console.log(airportsResult);
+
+  //   Gestione errori in fetch
+  if (countryResult.status === "rejected") {
+    console.error("❌ Errore nella fetch della country:", countryResult.reason);
+  }
+  if (meteoResult.status === "rejected") {
+    console.error("❌ Errore nella fetch del meteo:", meteoResult.reason);
+  }
+  if (airportsResult.status === "rejected") {
+    console.error(
+      "❌ Errore nella fetch degli aeroporti:",
+      airportResult.reason
+    );
+  }
+
+  // Gestione inserimento null
+  const countryData =
+    countryResult.status === "rejected" ? "null" : countryResult.value;
+
+  const meteoData =
+    meteoResult.status === "rejected" ? "null" : meteoResult.value;
+
+  const airportsData =
+    airportsResult.status === "rejected" ? "null" : airportsResult.value;
+
+  // Airport NOT FOUND
+  const airport = airportsData.find(
+    (a) => a.location.city.toLowerCase() === query.toLowerCase()
+  );
+  const airportName = airport ? airport.name : "Airport Not Found";
+
+  // ---- Creiamo l'array finale
+  const countryInformation = {
+    city: countryData.name,
+    country: countryData.country,
+    temperature: meteoData.temperature,
+    weather: meteoData.weather_description,
+    airportName,
+  };
+  // ---- Ritorniamo l'array finale
+  return countryInformation;
+}
+
+(async () => {
+  const countryInformation = await getDashboardData(`London`);
+  console.log(countryInformation);
+
+  console.log("FINE");
+})();
